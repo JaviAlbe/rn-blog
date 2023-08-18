9e0a17ad891b51b58eb31c445ce7c9f5c796a9b6
Javier Albeniz <j.albenizlopez@gmail.com>
2023-08-18 17:06:00 +0200

    Context added to the project to manage state without using props

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import IndexScreen from "./src/IndexScreen";
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const navigator = createStackNavigator({
-    Index: IndexScreen
-}, {
-    initialRouteName:'Index',
-    defaultNavigationOptions: {
-        title: 'Blogs'
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
+
+const navigator = createStackNavigator(
+    {
+        Index: IndexScreen,
+    },
+    {
+        initialRouteName: 'Index',
+        defaultNavigationOptions: {
+            title: 'Blogs',
+        },
     }
-})
+);
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator);
+
+export default () => {
+    return (
+        <BlogProvider>
+            <App />
+        </BlogProvider>
+    );
+};
