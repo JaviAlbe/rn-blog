9c7bef0f18493b492f11e8fedd61457354bcd967
Javier Albeniz <j.albenizlopez@gmail.com>
2023-08-21 13:02:46 +0200

    Blog posts can now be added using Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+
+    const [blogPosts, setBlogPosts] = useState([])
+
+    const addBlogPost = () => {
+        //takes the current blogPosts and adds another post at the end of the array
+        setBlogPosts([
+            ...blogPosts ,
+            { title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+
     return (
-        <BlogContext.Provider value={[1, 2, 3]}>{children}</BlogContext.Provider>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+            {children}
+        </BlogContext.Provider>
     );
 };
 
