9d4a6ba8445d803ddd62e457f64a84ca7f70af81
Javier Albeniz <j.albenizlopez@gmail.com>
2023-09-11 18:36:03 +0100

    Blog posts can be created with title and description

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,10 +7,12 @@ const blogReducer = (state, action) => {
             return state.filter((blogPost) => blogPost.id !== action.payload )
 
         case 'add_blogpost':
-            return [...state,
+            return [
+                ...state,
                 {
                     id: Math.floor(Math.random() * 99999),
-                    title: `Blog Post #${state.length + 1}`
+                    title: action.payload.title,
+                    content: action.payload.content
                 }
             ]
         default:
@@ -19,8 +21,9 @@ const blogReducer = (state, action) => {
 }
 
 const addBlogPost = (dispatch) => {
-    return () => {
-        dispatch({ type: 'add_blogpost' })
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title, content } })
+        callback()
     }
 }
 
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import { Context } from '../context/BlogContext'
+import { EvilIcons } from "@expo/vector-icons";
+import IndexScreen from "./IndexScreen";
 
 const ShowScreen = ({ navigation }) => {
 
@@ -11,8 +13,19 @@ const ShowScreen = ({ navigation }) => {
     return(
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     )
+}
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+                <EvilIcons name="pencil" size={35} />
+            </TouchableOpacity>
+        ),
+    };
 }
 
 const styles = StyleSheet.create({
