6e0cc397a63096ccd4744686442a4af6c59ecc65
Javier Albeniz <j.albenizlopez@gmail.com>
2023-09-12 19:44:23 +0100

    Edit Screen created and added to the navigation object

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+const EditScreen = ({ navigation }) => {
+
+    return(
+        <View>
+            <Text>Edit Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default EditScreen
