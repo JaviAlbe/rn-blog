4ea725c4f6b19f8b498beb9e88a91ba68de95cb6
Javier Albeniz <j.albenizlopez@gmail.com>
2023-09-14 17:17:45 +0100

    Edit screen implemented and edit/create form extracted to a component

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from "react";
-import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import React, { useContext } from "react";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../Components/BlogPostForm";
 
 
 const EditScreen = ({ navigation }) => {
@@ -9,38 +9,10 @@ const EditScreen = ({ navigation }) => {
 
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
 
-    const [title, setTitle] =  useState(blogPost.title)
-
-    const [content, setContent] = useState(blogPost.content)
-
-    return(
-        <View>
-            <Text style={styles.label}>Edit Title:</Text>
-            <TextInput style={styles.input} value={title} onchangeText={(newTitle) => setTitle(newTitle)}/>
-            <Text style={styles.label}>Edit Content:</Text>
-            <TextInput style={styles.input} value={content} onchangeText={(newContent) => setContent(newContent)}/>
-            <Button
-                title='Submit'
-                onPress={() => console.log('Hello')}
-            />
-        </View>
-    )
+    return <BlogPostForm
+        initialValues={{ title: blogPost.title, content: blogPost.content }}
+        onSubmit={(newTitle, newContent) => console.log(newTitle, newContent)} />
 }
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize:18,
-        borderWidth:1,
-        borderColor:'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5,
-    },
-    label: {
-        fontSize:20,
-        marginBottom: 10,
-        marginLeft: 5,
-    }
-})
 
 export default EditScreen
