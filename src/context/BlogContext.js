932b27831631bd5c3c2bfb3d4470d93779abc213
Javier Albeniz <j.albenizlopez@gmail.com>
2023-09-14 14:23:24 +0100

    WiP: Working on the edit screen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -35,5 +35,5 @@ const deleteBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(blogReducer,
     {addBlogPost, deleteBlogPost},
-    [])
+    [{ title:'Test Post', content: 'Test content for this specific post', id: 1 }])
 
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,18 +1,46 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import { Context } from "../context/BlogContext";
 
 
 const EditScreen = ({ navigation }) => {
 
+    const { state } = useContext(Context)
+
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
+    const [title, setTitle] =  useState(blogPost.title)
+
+    const [content, setContent] = useState(blogPost.content)
+
     return(
         <View>
-            <Text>Edit Screen</Text>
+            <Text style={styles.label}>Edit Title:</Text>
+            <TextInput style={styles.input} value={title} onchangeText={(newTitle) => setTitle(newTitle)}/>
+            <Text style={styles.label}>Edit Content:</Text>
+            <TextInput style={styles.input} value={content} onchangeText={(newContent) => setContent(newContent)}/>
+            <Button
+                title='Submit'
+                onPress={() => console.log('Hello')}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    input: {
+        fontSize:18,
+        borderWidth:1,
+        borderColor:'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+    },
+    label: {
+        fontSize:20,
+        marginBottom: 10,
+        marginLeft: 5,
+    }
 })
 
 export default EditScreen
